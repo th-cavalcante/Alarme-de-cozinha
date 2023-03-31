000e8f6de1d9898e673e9108db050659ce8ea18e
Thiago Cavalcante <th-fernandes@hotmail.com>
2023-03-31 10:28:14 -0300

    Alteração no Select minuto
    Acrescentei o minuto "1"

--- script.js
+++ script.js
@@ -1,60 +1,49 @@
-//Variaveis para o contador sec=segundos, min=minuto, hr=hora (todas iniciando do "0")
 let sec= 0
 let min= 0
 let hr= 0
-//Fim variaveis
 
-//Função contador (id#watch (relogio))
 function watch(){  
-   sec++// Contagem apartir do zero +1
-   if(sec == 60){ //Quando o segundo chegar a 60
-      min++ //Acrescente +1 no minuto
-      sec=0 // Consequentemente retorne o segundo para o "0", para iniciar uma nova contagem
-      if(min ==60){ //Quando o minuto chegar a 60
-          min=0 //Retorne o minuto ao "0"
-          hr++ //E acrescente +1 na hora.
+   sec++
+   if(sec == 60){ 
+      min++ 
+      sec=0 
+      if(min ==60){
+          min=0 
+          hr++ 
       } 
       controlMinute();
    }
-   //Print do Relogio --> 00:00:00 (esta dentro de uma div)
+
    document.getElementById('watch').innerText = twodigits(hr)+':'+twodigits(min)+':'+twodigits(sec)
 }
 
 
-//função para o contador manter os dois digitos(00:00:00)
-function twodigits(digit){ //Função para informar que o temporizador terá 2 digitos (00:00)
+
+function twodigits(digit){ 
    if(digit <10){
        return('0'+digit)
    }else{
        return(digit)
    }
 }
-//fim função
 
-
-//Função do botao iniciar
 function iniciar(){   
-   watch()//Ao clicar no botao iniciar, chamar função contador de tempo
-  interval = setInterval(watch,1000)  //intervalo de tempo do contador  
+   watch()
+  interval = setInterval(watch,1000) 
 }
-//fim 
 
-
-//Função 'PARAR' contador de tempo...
 function stop(){
-   clearInterval(interval)// limpa o intervalo de tempo
-   sec=0 //ao clicar em parar segundo voltar ao "0"
-   min=0 //ao clicar em parar minuto voltar ao "0"
-   document.getElementById('watch').innerText = `` //contador de tempo vai aparecer zerada dessa forma
-   msg.innerHTML = `` // msg de conclusão tbm apaga. 
+   clearInterval(interval)
+   sec=0 
+   min=0 
+   document.getElementById('watch').innerText = `` 
+   msg.innerHTML = `` 
      
 }
-//fim função parar
 
-//Função para pegar os numeros do select
 function controlMinute(){
-   let selectMin = document.querySelector('#selectMinute'); //id do select
-   let minute = selectMin.options[selectMin.selectedIndex].textContent; //Buscando valores dentro do select
+   let selectMin = document.querySelector('#selectMinute'); 
+   let minute = selectMin.options[selectMin.selectedIndex].textContent; 
    
    if(min == minute){ 
        let audio = document.querySelector('audio')
