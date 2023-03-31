ce8b9fec71eb5b2227ccb99c433f9c06337a5615
Thiago Cavalcante <th-fernandes@hotmail.com>
2023-03-31 10:56:24 -0300

    Alterações no CSS
    alterado efeitos dos botoes, e realizei a descompressão dos codigos. Titulo tbm foi modificado

--- script.js
+++ script.js
@@ -1,4 +1,59 @@
-let sec=0,min=0,hr=0;function watch(){sec++,60==sec&&(min++,sec=0,60==min&&(min=0,hr++),controlMinute()),document.getElementById("watch").innerText=twodigits(hr)+":"+twodigits(min)+":"+twodigits(sec)}function twodigits(a){return 10>a?"0"+a:a}function iniciar(){watch(),interval=setInterval(watch,1e3)}function stop(){clearInterval(interval),sec=0,min=0,document.getElementById("watch").innerText=``,msg.innerHTML=``}function controlMinute(){let a=document.querySelector("#selectMinute"),b=a.options[a.selectedIndex].textContent;if(min==b){let a=document.querySelector("audio"),b=document.getElementById("msg");stop(),a.play(),b.innerHTML=`<p id="mgsFinal">TEMPO FINALIZADO</p>`}}
+let sec= 0
+let min= 0
+let hr= 0
+
+function watch(){  
+   sec++
+   if(sec == 60){ 
+      min++ 
+      sec=0 
+      if(min ==60){
+          min=0 
+          hr++ 
+      } 
+      controlMinute();
+   }
+
+   document.getElementById('watch').innerText = twodigits(hr)+':'+twodigits(min)+':'+twodigits(sec)
+}
+
+
+
+function twodigits(digit){ 
+   if(digit <10){
+       return('0'+digit)
+   }else{
+       return(digit)
+   }
+}
+
+function iniciar(){   
+   watch()
+  interval = setInterval(watch,1000) 
+}
+
+function stop(){
+   clearInterval(interval)
+   sec=0 
+   min=0 
+   document.getElementById('watch').innerText = `` 
+   msg.innerHTML = `` 
+     
+}
+
+function controlMinute(){
+   let selectMin = document.querySelector('#selectMinute'); 
+   let minute = selectMin.options[selectMin.selectedIndex].textContent; 
+   
+   if(min == minute){ 
+       let audio = document.querySelector('audio')
+       let msg =  document.getElementById('msg')
+       stop() 
+       audio.play() 
+       msg.innerHTML = `<p id="mgsFinal">TEMPO FINALIZADO</p>`
+   }
+}
+
 
 
 
